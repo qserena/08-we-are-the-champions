9d8a628b27718133d5df22fb18e2ef4b5da75497
Tom Serenander <tom@serenander.se>
2024-02-22 15:43:50 +0100

    Added boostrap icons reference.

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
 import {
 	getDatabase,
 	ref,
 	push,
 	onValue,
 	remove,
-} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 const appSettings = {
 	databaseURL:
-		"https://champions-92a67-default-rtdb.europe-west1.firebasedatabase.app",
+		'https://champions-92a67-default-rtdb.europe-west1.firebasedatabase.app',
 }
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const endorsementsInDB = ref(database, "endorsements")
+const endorsementsInDB = ref(database, 'endorsements')
 
-const inputEl = document.getElementById("endorsement-input")
-const publishButton = document.getElementById("publish-button")
-const endorsementListEl = document.getElementById("endorsement-list")
+const inputEl = document.getElementById('endorsement-input')
+const publishButton = document.getElementById('publish-button')
+const endorsementListEl = document.getElementById('endorsement-list')
 
-publishButton.addEventListener("click", function () {
+publishButton.addEventListener('click', function () {
 	const endorsement = inputEl.value
-	if (endorsement !== "") {
+	if (endorsement !== '') {
 		push(endorsementsInDB, endorsement)
 		clearInputField()
 	}
@@ -37,7 +37,7 @@ onValue(endorsementsInDB, function (snapshot) {
 		for (let i = 0; i < endorsementArray.length; i++) {
 			let currentItem = endorsementArray[i]
 			let endorsementText = currentItem[1]
-			let newItem = document.createElement("li")
+			let newItem = document.createElement('li')
 			newItem.textContent = endorsementText
 			endorsementListEl.append(newItem)
 		}
@@ -45,9 +45,9 @@ onValue(endorsementsInDB, function (snapshot) {
 })
 
 function clearInputField() {
-	inputEl.value = ""
+	inputEl.value = ''
 }
 
 function clearEndorsementListEl() {
-	endorsementListEl.innerHTML = ""
+	endorsementListEl.innerHTML = ''
 }
